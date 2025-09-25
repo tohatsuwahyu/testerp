977d8ab38b7afe3b1c7e4529a017774c2ec4abef
tohatsuwahyu <tohatsu.wbp@gmail.com>
2025-09-25 17:49:27 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Ganti dengan API Key dan Spreadsheet ID Anda
+const CONFIG = {
+    API_KEY: "AIzaSyDoxaWt-6J3w-5jwrmTFlcDkrswgaGs9ik",
+    PRODUCTION_SHEET_ID: "1f0VPakpWfflzK1lEKFGkc7f7BQKLhfugYV6mkBRUlSo",
+    SHIPPING_SHEET_ID: "1_NiXTIBmm53osRnPLslCrSRE0PviW3Ce1_GsoyWckjw"
+};
